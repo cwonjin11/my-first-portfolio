2c8bcba2d62c3078375b2a1098069e927dd9926e
Wonjin Cho <cwonjin1@gmail.com>
2021-11-18 12:19:59 -0500

    Portfolio components update

--- src/data.js
+++ src/data.js
@@ -1,35 +1,35 @@
 export const projects = [
   {
     title: "Lego Displayer",
-    subtitle: "React Project",
+    subtitle: "React.js Project",
     description:
       "React front-end, Ruby on Rails as back-end SPA. Users can display, describe and share their Lego! Full CRUD with search function included 🕌",
     image: "./project-5.gif",
     link: "https://drive.google.com/file/d/12tC1mxwQ1V_FhXtfgZ-8R8ry3aLe0ocW/view?usp=sharing",
   },
   {
-    title: "React Tracks",
-    subtitle: "React and Python",
+    title: "Dinosaur Flash Card",
+    subtitle: "Javascript and Ruby on Rails",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-2.gif",
-    link: "https://reactbootcamp.com",
+      "This Single Page Application(SPA) shows dinosaurs cards by when Era. Click each Mesozoic Era to find associated dinosaurs 🦕!" ,
+    image: "./project-4.png",
+    link: "https://drive.google.com/file/d/1K6elB_7EcpeyMndoADfOWIe2EH-KV9u3/view",
   },
   {
-    title: "DevChat",
-    subtitle: "React and Firebase",
+    title: "Rate my Hairstylist",
+    subtitle: "Ruby on Rails",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://reactbootcamp.com",
+      "Rails based web app to allow users creating reviews along with hairstylist. Full CRUD, Search funtion, OmniAuth for Log in authentication applied",
+    image: "./project-3.png",
+    link: "https://drive.google.com/file/d/1Qe1-ygtGggFIlaVsYm8m4GS6fW96FGp3/view",
   },
   {
-    title: "Epic Todo App",
-    subtitle: "React Hooks",
+    title: "Vehicle Organizer",
+    subtitle: "Ruby Sinatra web application",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-4.gif",
-    link: "https://reactbootcamp.com",
+      "A simple Sinatra-powered web app created to keep tracking user's vehicles' make, model, year, type, color, vin# and price. Full CRUD funtion applied",
+    image: "./project-2.png",
+    link: "https://drive.google.com/file/d/1LP8sa1N1waOHuNjtfv_SAXtvKReinEiO/view?usp=sharing",
   },
 ];
 
