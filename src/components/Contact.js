75a801c4aa64728b58183734eef9f82c0fee012c
Wonjin Cho <cwonjin1@gmail.com>
2021-11-18 14:09:05 -0500

    small change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -39,12 +39,11 @@ export default function Contact() {
             marginHeight={0}
             marginWidth={0}
             style={{ filter: "opacity(0.7)" }}
-            // src="https://www.google.com/maps/embed/v1/place?q=97+warren+st+new+york+city&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
             src="https://www.google.com/maps/embed/v1/place?q=8+powderview+ct&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
           <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
-              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">  
                 ADDRESS
               </h2>
               <p className="mt-1">
